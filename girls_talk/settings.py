3a618b87c99022162c06fb0e03bb2217ab0b8c5b
Sanvi Singh <sanvis@akamai.com>
2018-03-09 00:08:51 +0530

    djangogirls

--- girls_talk/settings.py
+++ girls_talk/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +25,7 @@ SECRET_KEY = 'j-gdk6ccyi#96i&23&9j+cdedyag!p65_77+n1ylbz7#tv)+3p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.18.98.176']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'girls_talk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- talks/admin.py
+++ talks/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Genre)
-admin.site.register(Notification)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Votes)
+#admin.site.register(User)
+#admin.site.register(Genre)
+#admin.site.register(Notification)
+#admin.site.register(Question)
+#admin.site.register(Answer)
+#admin.site.register(Votes)
