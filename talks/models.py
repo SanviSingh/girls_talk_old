2328906d67df3d94fa9a7bee6dd1f3b56478701f
SanviSingh <singh.sanvi8@gmail.com>
2018-03-08 09:59:37 -0500

    almost ready

--- talks/models.py
+++ talks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
     userid=models.AutoField(primary_key=True)
     name=models.CharField(max_length=200)
-    email=models.CharField(max_length=500)
+    email=models.CharField(max_length=500,unique=True)
     password=models.CharField(max_length=20)
 
     def register():
@@ -14,3 +14,45 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    genreid = models.AutoField(primary_key=True)
+    gname   = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.gname
+
+class Notification(models.Model):
+    eventid  = models.AutoField(primary_key=True)
+    event    = models.TextField()
+    user     = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    timestamp= models.DateTimeField(auto_now=True)
+
+class Question(models.Model):
+    qid      = models.AutoField(primary_key=True)
+    user     = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    qtext    = models.TextField()
+    qdesc    = models.TextField()
+    genre    = models.ForeignKey(Genre, blank=True, null=True, on_delete=models.SET_NULL)
+    hcount   = models.IntegerField()
+    timestamp= models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return (self.qtext)
+
+class Answer(models.Model):
+    aid     = models.AutoField(primary_key=True)
+    question= models.ForeignKey(Question, blank=True, null=True, on_delete=models.SET_NULL)
+    user    = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    atext   = models.TextField()
+    likes   = models.IntegerField()
+    timestamp= models.DateTimeField(auto_now_add=True,null=True) 
+    
+    def __str__(self):
+        return self.atext
+
+class Votes(models.Model):
+    answer = models.ForeignKey(Answer , blank=True, null=True, on_delete=models.SET_NULL)
+    user   = models.ForeignKey(User , blank=True, null=True, on_delete=models.SET_NULL)
+
